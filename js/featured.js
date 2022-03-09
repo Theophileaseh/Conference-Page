93ffa6d8ff8e84375cb032bd0261bbcc0fc66e7b
Theophileaseh <theophilenwachan@gmail.com>
2022-03-09 16:17:24 +0100

    Fix Linter Errors

--- js/featured.js
+++ js/featured.js
@@ -1,30 +1,29 @@
 const SpeakersOne = [
-    {
-      name: 'Albert Brown',
-      position: 'CEO, HughWoods',
-      description: 'kdfjnvjlgbdentgjlbntrbgl jl tj44/jn4k5 44k5ntk 4tj 4',
-      featured_image: 'p.png',
-    },
-    {
-      name: 'Katherine Key',
-      position: 'COO, Irving',
-      description: 'kdfjnvjlgbdentgjlbntrbgl jl tj44/jn4k5 44k5ntk 4tj 4',
-      featured_image: 'p.png',
-    },
-    {
-      name: 'Ernest Backman',
-      position: 'Google Developer Expert',
-      description: 'kdfjnvjlgbdentgjlbntrbgl jl tj44/jn4k5 44k5ntk 4tj 4',
-      featured_image: 'p.png',
-    },
+  {
+    name: 'Albert Brown',
+    position: 'CEO, HughWoods',
+    description: 'kdfjnvjlgbdentgjlbntrbgl jl tj44/jn4k5 44k5ntk 4tj 4',
+    featured_image: 'p.png',
+  },
+  {
+    name: 'Katherine Key',
+    position: 'COO, Irving',
+    description: 'kdfjnvjlgbdentgjlbntrbgl jl tj44/jn4k5 44k5ntk 4tj 4',
+    featured_image: 'p.png',
+  },
+  {
+    name: 'Ernest Backman',
+    position: 'Google Developer Expert',
+    description: 'kdfjnvjlgbdentgjlbntrbgl jl tj44/jn4k5 44k5ntk 4tj 4',
+    featured_image: 'p.png',
+  },
 
-  ];
+];
+const speakersOneContainer = document.getElementById('speakers-1');
   
-  const speakersOneContainer = document.getElementById('speakers-1');
-  
-  const allSpeakersOne = SpeakersOne
-    .map(
-      (speakersOnes) => `
+const allSpeakersOne = SpeakersOne
+  .map(
+    (speakersOnes) => `
       <div class="single-speaker">
       <div class="speaker-img">
           <img src="img/span-bg.png" class="img-bg">
@@ -37,41 +36,36 @@ const SpeakersOne = [
       </div>
   </div>
     `,
-    ).join('');
+  ).join('');
   
-  speakersOneContainer.innerHTML = allSpeakersOne;  
+speakersOneContainer.innerHTML = allSpeakersOne;
 
+// For second set of speakers
 
-  // For second set of speakers
-
-
-  const SpeakersTwo = [
-    {
-      name2: 'Kilnam Chon',
-      position2: 'Executive Director',
-      description2: 'kdfjnvjlgbdentgjlbntrbgl jl tj44/jn4k5 44k5ntk 4tj 4',
-      featured_image2: 'p.png',
-    },
-    {
-      name2: 'Julia Leda',
-      position2: 'President of Young Pirates of Europe',
-      description2: 'kdfjnvjlgbdentgjlbntrbgl jl tj44/jn4k5 44k5ntk 4tj 4',
-      featured_image2: 'p.png',
-    },
-    {
-      name2: 'Angelica Bradley',
-      position2: 'Creative Director at Cards',
-      description2: 'kdfjnvjlgbdentgjlbntrbgl jl tj44/jn4k5 44k5ntk 4tj 4',
-      featured_image2: 'p.png',
-    }, 
-
-  ];
-  
-  const speakersTwoContainer = document.getElementById('speakers-2');
-  
-  const allSpeakersTwo = SpeakersTwo
-    .map(
-      (speakersTwos) => `
+const SpeakersTwo = [
+  {
+    name2: 'Kilnam Chon',
+    position2: 'Executive Director',
+    description2: 'kdfjnvjlgbdentgjlbntrbgl jl tj44/jn4k5 44k5ntk 4tj 4',
+    featured_image2: 'p.png',
+  },
+  {
+    name2: 'Julia Leda',
+    position2: 'President of Young Pirates of Europe',
+    description2: 'kdfjnvjlgbdentgjlbntrbgl jl tj44/jn4k5 44k5ntk 4tj 4',
+    featured_image2: 'p.png',
+  },
+  {
+    name2: 'Angelica Bradley',
+    position2: 'Creative Director at Cards',
+    description2: 'kdfjnvjlgbdentgjlbntrbgl jl tj44/jn4k5 44k5ntk 4tj 4',
+    featured_image2: 'p.png',
+  },
+];
+const speakersTwoContainer = document.getElementById('speakers-2');
+const allSpeakersTwo = SpeakersTwo
+  .map(
+    (speakersTwos) => `
       <div class="single-speaker">
       <div class="speaker-img">
           <img src="img/span-bg.png" class="img-bg">
@@ -84,6 +78,5 @@ const SpeakersOne = [
       </div>
   </div>
     `,
-    ).join('');
-  
-  speakersTwoContainer.innerHTML = allSpeakersTwo; 
+  ).join('');s
+speakersTwoContainer.innerHTML = allSpeakersTwo;
